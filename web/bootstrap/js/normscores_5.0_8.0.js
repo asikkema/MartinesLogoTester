af082390f6e9f93f460c31edd1e595afa976b218
Albert Sikkema <albert.sikkema@uppenergy.com>
2013-05-13 15:05:16 +0200

    latest stuff

--- web/bootstrap/js/normscores_5.0_8.0.js
+++ web/bootstrap/js/normscores_5.0_8.0.js
@@ -1549,799 +1549,351 @@ var normScores50 = {
             percentiel: 57.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 104,
-            percentiel: 60.5
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 43,
+            to: 43,
             normScore: 105,
             percentiel: 63.1
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 106,
-            percentiel: 65.5
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 44,
+            to: 44,
             normScore: 107,
             percentiel: 68.0
         },
         {
-            from: 4,
-            to: 12,
+            from: 45,
+            to: 45,
             normScore: 108,
             percentiel: 70.3
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 109,
-            percentiel: 72.6
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 46,
+            to: 46,
             normScore: 110,
             percentiel: 74.8
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 111,
-            percentiel: 76.8
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 47,
+            to: 47,
             normScore: 112,
             percentiel: 78.8
         },
         {
-            from: 4,
-            to: 12,
+            from: 48,
+            to: 48,
             normScore: 113,
             percentiel: 80.7
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 114,
-            percentiel: 82.5
         },{
-            from: 4,
-            to: 12,
+            from: 49,
+            to: 49,
             normScore: 115,
             percentiel: 84.1
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 116,
-            percentiel: 85.7
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 50,
+            to: 50,
             normScore: 117,
             percentiel: 87.1
         },
         {
-            from: 4,
-            to: 12,
+            from: 51,
+            to: 51,
             normScore: 118,
             percentiel: 88.5
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 119,
-            percentiel: 89.7
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 52,
+            to: 52,
             normScore: 120,
             percentiel: 90.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 121,
-            percentiel: 91.9
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 53,
+            to: 53,
             normScore: 122,
             percentiel: 92.9
         },
         {
-            from: 4,
-            to: 12,
+            from: 54,
+            to: 54,
             normScore: 123,
             percentiel: 93.7
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 124,
-            percentiel: 94.5
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 55,
+            to: 55,
             normScore: 125,
             percentiel: 95.2
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 126,
-            percentiel: 95.8
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 56,
+            to: 56,
             normScore: 127,
             percentiel: 96.4
         },
         {
-            from: 4,
-            to: 12,
+            from: 57,
+            to: 57,
             normScore: 128,
             percentiel: 96.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 129,
-            percentiel: 97.3
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 58,
+            to: 58,
             normScore: 130,
             percentiel: 97.7
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 131,
-            percentiel: 98.1
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 59,
+            to: 59,
             normScore: 132,
             percentiel: 98.4
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 133,
-            percentiel: 98.6
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 60,
+            to: 60,
             normScore: 134,
             percentiel: 98.8
         },
         {
-            from: 4,
-            to: 12,
+            from: 61,
+            to: 61,
             normScore: 135,
             percentiel: 99.0
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 136,
-            percentiel: 99.2
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 62,
+            to: 62,
             normScore: 137,
             percentiel: 99.3
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 138,
-            percentiel: 99.4
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 63,
+            to: 63,
             normScore: 139,
             percentiel: 99.5
         },
         {
-            from: 4,
-            to: 12,
+            from: 64,
+            to: 64,
             normScore: 140,
             percentiel: 99.6
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 141,
-            percentiel: 99.7
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 65,
+            to: 65,
             normScore: 142,
             percentiel: 99.7
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 143,
-            percentiel: 99.8
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 66,
+            to: 66,
             normScore: 144,
             percentiel: 99.8
         },
         {
-            from: 4,
-            to: 12,
+            from: 67,
+            to: 76,
             normScore: 145,
             percentiel: 99.9
         }
     ],
     wgi: [
         {
-            from: 4,
-            to: 12,
+            from: 2,
+            to: 5,
             normScore: 55,
             percentiel: 0.1
         },
         {
-            from: -1,
-            to: -1,
+            from: 6,
+            to: 6,
             normScore: 56,
             percentiel: 0.2
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 57,
-            percentiel: 0.2
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 58,
-            percentiel: 0.3
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 7,
+            to: 7,
             normScore: 59,
             percentiel: 0.3
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 60,
-            percentiel: 0.4
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 61,
-            percentiel: 0.5
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 62,
-            percentiel: 0.6
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 8,
+            to: 8,
             normScore: 63,
             percentiel: 0.7
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 64,
-            percentiel: 0.8
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 65,
-            percentiel: 1.0
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 9,
+            to: 9,
             normScore: 66,
             percentiel: 1.2
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 67,
-            percentiel: 1.4
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 68,
-            percentiel: 1.6
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 10,
+            to: 10,
             normScore: 69,
             percentiel: 1.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 70,
-            percentiel: 2.3
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 71,
-            percentiel: 2.7
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 72,
-            percentiel: 3.1
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 11,
+            to: 11,
             normScore: 73,
             percentiel: 3.6
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 74,
-            percentiel: 4.2
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 75,
-            percentiel: 4.8
-        },
-        {
-            from: 4,
+            from: 12,
             to: 12,
             normScore: 76,
             percentiel: 5.5
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 77,
-            percentiel: 6.3
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 78,
-            percentiel: 7.1
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 13,
+            to: 13,
             normScore: 79,
             percentiel: 8.1
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 80,
-            percentiel: 9.1
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 81,
-            percentiel: 10.3
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 14,
+            to: 14,
             normScore: 82,
             percentiel: 11.5
         },
-        {
-            from: 4,
-            to: 12,
-            normScore: 83,
-            percentiel: 12.9
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 84,
-            percentiel: 14.3
-        },{
-            from: 4,
-            to: 12,
+		{
+            from: 15,
+            to: 15,
             normScore: 85,
             percentiel: 15.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 86,
-            percentiel: 17.5
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 87,
-            percentiel: 19.3
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 16,
+            to: 16,
             normScore: 88,
             percentiel: 21.2
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 89,
-            percentiel: 23.2
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 90,
-            percentiel: 52.2
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 17,
+            to: 17,
             normScore: 91,
             percentiel: 27.4
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 92,
-            percentiel: 29.7
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 93,
-            percentiel: 32.0
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 94,
-            percentiel: 34.5
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 18,
+            to: 18,
             normScore: 95,
             percentiel: 36.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 96,
-            percentiel: 39.5
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 97,
-            percentiel: 42.1
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 19,
+            to: 19,
             normScore: 98,
             percentiel: 44.7
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 99,
-            percentiel: 47.3
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 100,
-            percentiel: 50.0
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 20,
+            to: 20,
             normScore: 101,
             percentiel: 52.7
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 102,
-            percentiel: 55.3
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 21,
+            to: 21,
             normScore: 103,
             percentiel: 57.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 104,
-            percentiel: 60.5
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 105,
-            percentiel: 63.1
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 22,
+            to: 22,
             normScore: 106,
             percentiel: 65.5
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 107,
-            percentiel: 68.0
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 108,
-            percentiel: 70.3
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 23,
+            to: 23,
             normScore: 109,
             percentiel: 72.6
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 110,
-            percentiel: 74.8
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 111,
-            percentiel: 76.8
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 24,
+            to: 24,
             normScore: 112,
             percentiel: 78.8
         },
-        {
-            from: 4,
-            to: 12,
-            normScore: 113,
-            percentiel: 80.7
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 114,
-            percentiel: 82.5
-        },{
-            from: 4,
-            to: 12,
+		{
+            from: 25,
+            to: 25,
             normScore: 115,
             percentiel: 84.1
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 116,
-            percentiel: 85.7
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 117,
-            percentiel: 87.1
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 26,
+            to: 26,
             normScore: 118,
             percentiel: 88.5
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 119,
-            percentiel: 89.7
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 27,
+            to: 27,
             normScore: 120,
             percentiel: 90.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 121,
-            percentiel: 91.9
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 122,
-            percentiel: 92.9
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 28,
+            to: 28,
             normScore: 123,
             percentiel: 93.7
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 124,
-            percentiel: 94.5
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 125,
-            percentiel: 95.2
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 29,
+            to: 29,
             normScore: 126,
             percentiel: 95.8
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 127,
-            percentiel: 96.4
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 30,
+            to: 30,
             normScore: 128,
             percentiel: 96.9
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 129,
-            percentiel: 97.3
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 130,
-            percentiel: 97.7
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 31,
+            to: 31,
             normScore: 131,
             percentiel: 98.1
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 132,
-            percentiel: 98.4
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 133,
-            percentiel: 98.6
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 32,
+            to: 32,
             normScore: 134,
             percentiel: 98.8
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 135,
-            percentiel: 99.0
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 33,
+            to: 33,
             normScore: 136,
             percentiel: 99.2
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 137,
-            percentiel: 99.3
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 138,
-            percentiel: 99.4
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 34,
+            to: 34,
             normScore: 139,
             percentiel: 99.5
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 140,
-            percentiel: 99.6
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 35,
+            to: 35,
             normScore: 141,
             percentiel: 99.7
         },
         {
-            from: 4,
-            to: 12,
-            normScore: 142,
-            percentiel: 99.7
-        },
-        {
-            from: 4,
-            to: 12,
-            normScore: 143,
-            percentiel: 99.8
-        },
-        {
-            from: 4,
-            to: 12,
+            from: 36,
+            to: 36,
             normScore: 144,
             percentiel: 99.8
         },
         {
-            from: 4,
-            to: 12,
+            from: 37,
+            to: 38,
             normScore: 145,
             percentiel: 99.9
         }
